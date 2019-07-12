a8698670beda4dd4eb329f4646bf265d61db3536
Osman Karaketir <osmankaraketir@gamil.com>
2019-07-12 14:30:25 +0300

    Fix encoding

--- tst/test.js
+++ tst/test.js
@@ -1,12 +1,14 @@
 const restify = require('restify');
 const server = restify.createServer();
 const mongoose = require('mongoose');
-const rp = require("request-promise");
+const rp = require("request-promise").defaults({encoding: "utf8"});
 const cheerio = require('cheerio');
 const Promise = require("bluebird");
 const co = Promise.coroutine;
-
-
+const legacy = require("legacy-encoding");
+const unirest = require("unirest");
+// const up = Promise.promisifyAll(unirest);
+// const https = require('https');
 
 let con = mongoose.connect("mongodb://mongo:27017/db",
 { useNewUrlParser: true,
@@ -111,87 +113,143 @@ let updateURLs = co(function* () {
 });
 // updateURLs();
 //
-let updateDB = co( function* () {
+let updateDB = function () {
 
-  let urls = yield Url.find().lean().exec();
-  // console.log("hello", urls);
-  // console.log("url", url);
-  for (let i in urls)
-  {
-    url = urls[i].url;
-    // url = "https://web.archive.org/web/20120418024313/http://www.sis.itu.edu.tr:80/tr/ders_programlari/LSprogramlar/prg.php?fb=AKM";
-    console.log("URL:", url);
-    try {
-      while(url)
+  Url.find().lean().exec(
+    function (err, urls) {
+      for (let i in urls)
       {
-        // console.log(url);
-        let data = null;
+        url = urls[i].url;
+        // url = "https://web.archive.org/web/20120418024313/http://www.sis.itu.edu.tr:80/tr/ders_programlari/LSprogramlar/prg.php?fb=AKM";
+        console.log("URL:", url);
         try {
-           data = yield rp(url);
+          function calc(urll)
+          {
+            // console.log(url);
+            if( ! urll) return;
+            console.log("in calc:", urll);
+            let data = null;
+            try {
+              unirest.get(urll)
+                .encoding(null)
+                .end(co(function *(d){
+                  console.log("HELLo");
+                  buff = d;
+                  let data = legacy.decode(buff.raw_body, "iso-8859-9");
+                  const $ = cheerio.load(data);
+                  let strDate = $("#body > table > tbody > tr:nth-child(1) > td > table:nth-child(2) > tbody > tr > td:nth-child(2) > span").text().split(" ");
+                  // console.log("ilk", strDate);
+                  let code = $("select option:selected").val();
+                  // console.log("CODE");
+                  // console.log(code);
+                  // return;
+                  let date = null;
+                  try
+                  {
+                    date = strDate[0] +"/"+ strDate[1][0] + "/" + code;
+                  }
+                  catch(e)
+                  {
+                    console.log(""+e);
+                    strDate = $("#AutoNumber1 > tbody > tr:nth-child(1) > td > p:nth-child(3) > font").text().split(" ");
+                    // console.log("ikinci", strDate);
+                    // return;
+                    date = strDate[4] +"/"+ strDate[5][0] + "/" + code;
+                  }
+                  console.log(date);
+                  let aPage = new Page({date: date, html:data});
+                  let aPageA = Promise.promisifyAll(aPage);
+                  yield aPageA.saveAsync().then(function ok() {
+                    console.log(`saved:${date}`);
+                    console.log(`saved:${url}`);
+                  }).catch(function (err) {
+                    console.log(""+err);
+                  });
+
+                  calc( $("#wm-ipp-inside > div:nth-child(1) > table > tbody > tr:nth-child(1) > td.n > table > tbody > tr.d > td.b > a").attr("href"));
+                  // console.log("html", html);
+                  // let $ = cheerio.load(html);
+                  // console.log("after cheerio", $.html("#body > table > tbody > tr:nth-child(1) > td > table:nth-child(3) > tbody > tr > td:nth-child(2) > table").toString("utf8"));
+                  // res.write($.html("#body > table > tbody > tr:nth-child(1) > td > table:nth-child(3) > tbody > tr > td:nth-child(2) > table").toString("utf8"));
+              }));
+            } catch (e) {
+              console.log(""+e);
+            }
+          }
+          calc(url);
         } catch (e) {
           console.log(""+e);
-        } finally {
-
+          // continue;
         }
-        // console.log("HELLO");
-        // console.log(data);
-
-        const $ = cheerio.load(data);
-        let strDate = $("#body > table > tbody > tr:nth-child(1) > td > table:nth-child(2) > tbody > tr > td:nth-child(2) > span").text().split(" ");
-        // console.log("ilk", strDate);
-        let code = $("select option:selected").val();
-        // console.log("CODE");
-        // console.log(code);
-        // return;
-        let date = null;
-        try
-        {
-          date = strDate[0] +"/"+ strDate[1][0] + "/" + code;
-        }
-        catch(e)
-        {
-          console.log(""+e);
-          strDate = $("#AutoNumber1 > tbody > tr:nth-child(1) > td > p:nth-child(3) > font").text().split(" ");
-          // console.log("ikinci", strDate);
-          // return;
-          date = strDate[4] +"/"+ strDate[5][0] + "/" + code;
-        }
-        console.log(date);
-        let aPage = new Page({date: date, html:data});
-        let aPageA = Promise.promisifyAll(aPage);
-        yield aPageA.saveAsync().then(function ok() {
-          console.log(`saved:${date}`);
-          console.log(`saved:${url}`);
-        }).catch(function (err) {
-          console.log(""+err);
-        });
-
-        url = $("#wm-ipp-inside > div:nth-child(1) > table > tbody > tr:nth-child(1) > td.n > table > tbody > tr.d > td.b > a").attr("href");
-
-      }
-    } catch (e) {
-      console.log(""+e);
-      continue;
-    }
 
 
-  }
-  return "done";
-});
+      }//for
+      // console.log("end of for");
+      return "Pending";
+  });
+  // console.log("hello", urls);
+  // console.log("url", url);
+  // console.log("end of updateDB()");
+  return "Pending";
+};
 // updateDB();
 server.listen(3000);
 
 server.get("/", function (req, res, next) {
-        Url.find().lean().exec(function (err, one) {
+        // Url.find().lean().exec(function (err, one) {
+        //
+        //   res.send(one);
+        // });
 
-          res.send(one);
+        // var req = https.request({hostname: "https://web.archive.org/web/20190618105926/http://www.sis.itu.edu.tr/tr/ders_programlari/LSprogramlar/prg.php?fb=AKM"}, function (res) {
+    //     res.setEncoding('utf8');
+    //     console.log("RES", res);
+    //     var body = '';
+    //
+    //     res.on('data', function (chunk) {
+    //       body = body + chunk;
+    //     });
+    //
+    //     res.on('end',function(){
+    //       console.log("Body :" + body);
+    //       if (res.statusCode != 200) {
+    //         callback("Api call failed with response code " + res.statusCode);
+    //       } else {
+    //         callback(null);
+    //       }
+    //     });
+    //
+    //   });
+    //       req.on('error', function (e) {
+    //   console.log("Error : " + e.message);
+    //   callback(e);
+    // });
+        Page.findOne().lean().exec(function (err, data) {
+
+          // console.log("DATA", data);
+
+          let html = data.html;
+          // console.log("html", html);
+          let $ = cheerio.load(html);
+          // console.log("after cheerio", $.html("#body > table > tbody > tr:nth-child(1) > td > table:nth-child(3) > tbody > tr > td:nth-child(2) > table").toString("utf8"));
+          let notOld = $.html("#body > table > tbody > tr:nth-child(1) > td > table:nth-child(3) > tbody > tr > td:nth-child(2) > table").toString("utf8");
+          if(notOld)
+            res.write(notOld);
+          else res.write($.html("#AutoNumber1 > tbody > tr:nth-child(2) > td:nth-child(3) > b > b > table").toString("utf8"))
+            // let html = data.html;
+
+
         });
 
-        // res.send(values);
-      });
-server.get("/updateDB", function (req, res, next) {
+        // res.end();
+        // ress.send("hmm");
+});
 
+server.get("/updateDB", function (req, res, next) {
+  res.send(updateDB());
+  next();
 });
 server.get("/updateURLs", function (req, res, next) {
   res.send(updateURLs());
+  next();
 });
